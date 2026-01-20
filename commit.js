53697690739c4aa6d4d82031ee9ecbcd634578d3
nato-git <sr.nato710@gmail.com>
2026-01-20 14:34:01 +0900

    タイトル変更

--- commit.js
+++ commit.js
@@ -1,6 +1,7 @@
 const html = document.getElementById('textContent');
 
 async function load(){
+    //誰も入力せんといてね
     const csvFile = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTF8mFxOzq5hJSPyxjI7N1PbaYy0wZzy_r7XIIsJbm5lFWJ0EauGdmh50vwlS1ivOah6X5q3awclPDn/pub?output=csv');
     const csvtext = await csvFile.text();
     const rows = csvtext.split('\n');
